8f6746525ddfb91f5d4968f82f60feaa2844df5a
yazidalg <yazid.al2418@gmail.com>
2024-05-06 01:03:34 +0700

    fix conflict the folder

---
+++ TestTheLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using LibararyMod10;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaan = { -1, -3, -10 };
+            double[] akar = Aljabar.PersamaanAkarKuadrat(persamaan);
+            double[] hasil = { 2, -3 };
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(hasil);
+
+            Console.WriteLine("Akar Persamaan Kuadrat : ");
+            foreach (var akarValue in akar)
+            {
+                Console.WriteLine(akarValue);
+            }
+
+            Console.WriteLine("Hasil : ");
+            foreach (var value in hasilKuadrat)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
